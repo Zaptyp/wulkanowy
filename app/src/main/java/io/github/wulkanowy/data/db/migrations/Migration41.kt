c53f027477e8541c242d0088d21f827f17d6c5ce
luncenok <idziejczak3@gmail.com>
2021-09-29 20:15:13 +0200

    Add migration

---
+++ app/src/main/java/io/github/wulkanowy/data/db/migrations/Migration41.kt
@@ -0,0 +1,11 @@
+package io.github.wulkanowy.data.db.migrations
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+class Migration41 : Migration(40, 41) {
+
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE Timetable ADD COLUMN is_notified INTEGER NOT NULL DEFAULT 1")
+    }
+}
